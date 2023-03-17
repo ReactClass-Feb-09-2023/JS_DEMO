7ead4421ae5077f4f12b7338b7bb2d3ae1af5783
Amr Fouad <3mrfouad@gmail.com>
2023-03-16 20:25:35 -0600

    data types practice

--- script.js
+++ script.js
@@ -1,64 +1,76 @@
-// Data types
-// Primitive
+// select the correct variable declaration, and datatype / structure for the following
 
-// string, number, boolean, undefined, null, bigInt, Symbol
+// - email to, cc, subject, body
+// - email body
+// - calendar upcoming events, each event has title, time, and meeting-url
+// - products list, each product has a name, tagline (description), and stock-qty
+// - todo list, each todo has subject, status, start date, end date
 
-const name = 'John Doe' // string
-const age = 15 // number - integer
-const height = 180.5 // number - decimal
-const isAlive = true // boolean - true/false
-const fax = undefined // undefined
-const bankCredit = null // null absence
-const bankAccount = -9223372036854775n
-const mySymbol = Symbol('key')
-const z = 1
-const y = 2
-const x = z + y
-console.log(x)
+// Solutions:
 
-// Non Primitive
+// - email body
+const emailBody =
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore sunt, veritatis quod dicta accusamus illum at nesciunt repudiandae id fugiat incidunt maxime eaque excepturi iusto? Doloribus aut laboriosam esse laborum libero animi voluptate quam minus quidem, velit, excepturi voluptatibus commodi eum amet facilis ab distinctio perspiciatis nobis. Magni, eos quo.'
 
-// Array (list)
-const names = ['John', 'Jahn']
-const ages = [10, 19]
-const stuff = ['John', 16, false, 10.3]
-names[0] = 'William'
-
-// Objects
-const person = {
-  name: 'John',
-  age: 30,
-  email: 'john.doe@email.com',
-  telephone: '78012345698',
-  address: '123 12 ST Toronto'
+// email to, cc, subject, body
+const email = {
+  to: ['john.doe@email.com', 'Lili@email.com'],
+  cc: ['william@email.com', 'max@email.com'],
+  subject: 'Lorem, ipsum dolor.',
+  body: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique rem ad optio eius officia, doloribus rerum eligendi.'
 }
 
-console.log(person['name'])
-console.log(person.name)
-console.log(person)
+// - calendar upcoming events, each event has title, time, and meeting-url
+const Event1 = new Map()
+Event1.set('uuid', 'id2...........')
+Event1.set('name', 'event1')
+Event1.set('time', '17:00')
+Event1.set('url', 'https://daturl.com/datpage.html')
 
-// Map
-const personMap = new Map()
-personMap.set('name', 'Max')
-personMap.set('tel', '7801234669')
-personMap.set('age', 25)
+const Event2 = new Map()
+Event1.set('uuid', 'id2...........')
+Event1.set('name', 'event2')
+Event1.set('time', '16:00')
+Event1.set('url', 'https://datsameurl.com/datpage.html')
 
-console.log(personMap.get('name'))
+const calendar = new Set()
+calendar.add(Event1)
+calendar.add(event2)
 
-console.log(personMap)
+// - products list, each product has a name, tagline (description), and stock-qty
+let products = [
+  {
+    id: '0c4d40a6-ca4c-46e6-942e-5477e8c7f473',
+    name: 'Product A',
+    tagline: 'Description of Product A',
+    stockQty: 10
+  },
+  {
+    id: 'cf13fc94-03fb-4415-a3e5-b74038ea7ea9',
+    name: 'Product B',
+    tagline: 'Description of Product B',
+    stockQty: 5
+  },
+  {
+    id: '357ea31e-fa42-4d4e-9565-ad7295b2f1de',
+    name: 'Product C',
+    tagline: 'Description of Product C',
+    stockQty: 2
+  }
+]
 
-// Set
-const setOfNumbers = new Set()
-setOfNumbers.add(1)
-setOfNumbers.add(2)
-setOfNumbers.add(2)
-setOfNumbers.add(1)
-
-const emptyObj1 = {}
-const emptyObj2 = {}
-
-setOfNumbers.add(emptyObj1)
-setOfNumbers.add(emptyObj2)
-
-console.log(setOfNumbers.has())
-console.log(setOfNumbers)
+// - todo list, each todo has subject, status, start date, end date
+const todoList = [
+  {
+    subject: 'Lorem, ipsum dolor.',
+    isComplete: false,
+    startDate: '12/01/2023',
+    endDate: null
+  },
+  {
+    subject: 'Lorem, ipsum dolor.ssasfsdfdsafdsafsa',
+    isComplete: true,
+    startDate: '12/01/2023',
+    endDate: '13/01/2023'
+  }
+]
