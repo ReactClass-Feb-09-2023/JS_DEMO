f2ce84d68fc8dcee854e9052abdf8cda1bfad748
Amr Fouad <3mrfouad@gmail.com>
2023-03-21 21:05:08 -0600

    functions

--- script.js
+++ script.js
@@ -1,119 +1,33 @@
-// loops and iterations
+// DRY = Don't Repeat Yourself
 
-const persons = [
-  {
-    name: 'john',
-    email: ''
-  },
-  {
-    name: 'joe',
-    email: ''
-  },
-  {
-    name: 'jahn',
-    email: ''
-  },
-  {
-    name: 'Kal',
-    email: ''
-  }
-]
+// function, methods
 
-// for loop
-const len = persons.length //4
-
-for (let i = 0; i < len; i++) {
-  // console.log(`Hello ${persons[i].name}`)
+// function declaration
+function greet(prefix, name) {
+  if (typeof prefix !== 'string' || typeof name !== 'string')
+    throw new TypeError(
+      `calling greet function with ${typeof prefix} and ${typeof name}. greet expects all string args.`
+    )
+  console.log(`Hello from greeting ${prefix} ${name}`)
 }
 
-// while
-
-let i = 0
-while (i < len) {
-  // console.log(`Hello from while loop, ${persons[i].name}`)
-  i++
-  // console.log('i: ', i)
+const greet2 = function () {
+  console.log('greet 2 has been called')
 }
 
-// do-while
-
-let j = 3
-do {
-  // console.log(`Hello from do-while loop, ${persons[j].name}`)
-  j++
-  // console.log('i: ', j)
-} while (j < len)
-
-// for of (array)
-
-const nums = [10, 13, 16, 30]
-
-for (let num of nums) {
-  // console.log(num)
+const greet3 = () => {
+  return 'string from greet3'
 }
 
-// for in (object)
-
-const obj = {
-  name: 'joe',
-  age: 15,
-  email: 'joe@email',
-  address: '123 st'
+const sum = (x, y) => {
+  return x + y
 }
 
-for (let key in obj) {
-  // console.log(key, obj[key])
-}
+// function invocation
+// greet(true, false)
+const ret = greet3()
+console.log(ret)
+// greet3()
 
-// array methods
-
-const alphabets = ['a', 'b', 'c', 'd']
-
-alphabets.forEach((char, i, arr) => {
-  // console.log(char)
-  // console.log(i)
-  // console.log(arr)
-})
-
-// Practice
-// having array of numbers, print to the console only the even numbers
-
-const numbers = [
-  9, 8, 3, 7, 12, 6, 20, 5, 2, 1, 17, 4, 10, 11, 15, 14, 16, 13, 19, 18
-]
-
-for (let number of numbers) {
-  if (number % 2 !== 0) {
-    // console.log(number)
-  }
-}
-
-// having array of names, or people objects, print to the console a greeting message, with the person name
-const people = [
-  { name: 'Emily' },
-  { name: 'Jacob' },
-  { name: 'Sophia' },
-  { name: 'Ethan' },
-  { name: 'Olivia' },
-  { name: 'William' },
-  { name: 'Ava' },
-  { name: 'Michael' },
-  { name: 'Mia' },
-  { name: 'Benjamin' }
-]
-
-people.forEach(person => {
-  // console.log(`Hello, ${person.name}!`)
-})
-
-// having array of 5 strings, concatenate all the strings into one string and print to the console.
-
-const strs = ['H', 'e', 'l', 'l', 'o']
-
-let outputStr = ''
-
-for (let i = 0; i < strs.length; i++) {
-  outputStr += strs[i]
-}
-
-console.log(outputStr)
+const result = sum(4, 5)
+console.log(result)
