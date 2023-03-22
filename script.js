bcb06a0ccd4e181077a74204bfb49469edbde0df
Amr Fouad <3mrfouad@gmail.com>
2023-03-21 18:44:58 -0600

    truthy vs falsy

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
-// Casting (Type conversion)
-// string to number
-console.log(1 === Number('1'))
-console.log(Number('r'))
-console.log(typeof parseInt('10', 10))
-
-// to string
-console.log(String(true)) //'true'
-console.log(true)
-
 // to boolean
-// console.log(Boolean())
+console.log(Boolean())
+
+// truthy vs. falsy
+
+// numbers
+// 0, NaN => false
+// any other number => true
+console.log(Boolean(0))
+console.log(Boolean(NaN))
+console.log(Boolean(3))
+
+// strings
+// '' empty string => false
+// any other char => true
+console.log(Boolean(''))
+console.log(Boolean('aa'))
+
+// undefined, null => false
+console.log(Boolean(undefined))
+console.log(Boolean(null))
+
+// arrays, objects => true
+console.log(Boolean([]))
+console.log(Boolean({}))
+
+// check empty array
+const arr = ['a', 'b']
+console.log(Boolean(arr.length))
+
+// check empty objects
+const obj = {
+  key1: 'val1'
+}
+console.log(Boolean(Object.keys(obj).length))
+
+// booleans true, false
+
+// !! double bang
+
+console.log(Boolean(0))
+console.log(!!3)
