62bdf39f0c51dc3ad0512455fc7c3a7a06d6381b
Amr Fouad <3mrfouad@gmail.com>
2023-03-28 18:13:41 -0600

    demo creating forEach

--- script.js
+++ script.js
@@ -4,19 +4,15 @@
 
 // function declaration
 function greet(prefix, name) {
-  if (typeof prefix !== 'string' || typeof name !== 'string')
-    throw new TypeError(
-      `calling greet function with ${typeof prefix} and ${typeof name}. greet expects all string args.`
-    )
-  console.log(`Hello from greeting ${prefix} ${name}`)
+  console.log('THIS', this)
 }
 
 const greet2 = function () {
-  console.log('greet 2 has been called')
+  console.log('THIS', this)
 }
 
-const greet3 = () => {
-  return 'string from greet3'
+const greet3 = (x, y) => {
+  console.log('THIS', this)
 }
 
 const sum = (x, y) => {
@@ -25,9 +21,37 @@ const sum = (x, y) => {
 
 // function invocation
 // greet(true, false)
-const ret = greet3()
-console.log(ret)
+// const ret = greet3()
+// console.log(ret)
 // greet3()
 
-const result = sum(4, 5)
-console.log(result)
+// const result = sum(4, 5)
+// console.log(result)
+
+// Callback functions
+
+// const arr = [10, 20, 30]
+
+// const arrayIterationCB = (e, i, array) => {
+//   console.log(e, i, array)
+// }
+
+// arr.forEach(arrayIterationCB)
+
+// implement a function called forEach that accepts a callback function, and use it to iterate over the array elements and run the CB function.
+
+const demo_array = ['a', 'b', 'c']
+
+const myFunc = (e, i, a) => {
+  console.log(e, i, a)
+}
+
+const forEach = (arr, cb) => {
+  if (!Array.isArray(arr) || typeof cb !== 'function')
+    throw new TypeError('invalid data type')
+  for (let i = 0; i < arr.length; i++) {
+    cb(arr[i], i, arr)
+  }
+}
+
+forEach(demo_array, myFunc)
