8e49f39ca437394a98b73260b39cf84fb6d6c1ec
Amr Fouad <3mrfouad@gmail.com>
2023-03-28 19:56:56 -0600

    DOM API - basics

--- script.js
+++ script.js
@@ -1,24 +1,61 @@
-/**
- * @param {number} x desc x
- * @param {number} y desc y
- * @param {number} z desc z
- * @returns sum of all args (x+y+z)
- */
-const foo = (x, y, z) => {
-  if (typeof x !== 'number' || typeof y !== 'number' || typeof z !== 'number')
-    throw new Error(`${typeof x} received, expecting number`)
-  return x + y + z
-}
+// DOM
+// selecting an element from the documents
+const title = document.querySelector('#title')
+title.innerHTML = 'New Header'
+const paragraphs = document.querySelectorAll('p')
 
-// JS DOCS
+paragraphs.forEach(paragraph => {
+  paragraph.innerHTML = 'New Text'
+})
+console.log(paragraphs)
 
-// foo('3', 3, 3)
+// creating elements
+const newParagraph = document.createElement('p')
+newParagraph.innerText = 'Virtual P tag'
 
-try {
-  foo('3', 3, 3)
-} catch (error) {
-  console.error(error)
-  // do something else.
-} finally {
-  // run common code that runs regardless of what foo does
-}
+const body = document.querySelector('body')
+body.appendChild(newParagraph)
+
+const unorderedList = document.createElement('ul')
+const listItem1 = document.createElement('li')
+listItem1.innerHTML = 'list item #1'
+const listItem2 = document.createElement('li')
+listItem2.innerHTML = 'list item #2'
+body.appendChild(unorderedList)
+unorderedList.appendChild(listItem1)
+unorderedList.appendChild(listItem2)
+
+// Manipulating CSS
+
+title.style.backgroundColor = 'blue'
+title.style.color = 'white'
+title.style.cssText = `
+  font-size: 4rem;
+  color: red;
+`
+
+// Attributes
+
+const input = document.querySelector('input')
+input.setAttribute('type', 'date')
+input.removeAttribute('type')
+console.log(input.getAttribute('id'))
+
+// removing elements
+
+body.removeChild(input)
+
+// other ways to select elements
+
+const title = document.getElementById('title')
+// console.log(title)
+
+const paras = document.getElementsByClassName('para')
+// console.log(paras)
+
+const tags = document.getElementsByTagName('input')
+// console.log(tags)
+
+// classes
+
+console.log(paras[0].classList)
